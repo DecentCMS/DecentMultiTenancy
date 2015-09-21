d2640b699ff15a8e68dd9fc780d9fd859d15f44b
Bertrand Le Roy <beleroy@microsoft.com>
2015-09-20 20:05:34 -0700

    Package index points to Tenant.

--- index.js
+++ index.js
@@ -1,6 +1,4 @@
 // Decent Multi-Tenancy (c) 2014-2015 Bertrand Le Roy, under MIT. See LICENSE.txt for licensing details.
 'use strict';
 
-module.exports = {
-  Tenant: require('./lib/tenant')
-};
+module.exports = require('./lib/tenant');
